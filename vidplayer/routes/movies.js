b35a4fac8a916c4234116e1cd637ad6b4c6fe3b9
Venkata Sai Krishna Chaitanya Atluri <vxa190013@utdallas.edu>
2020-12-13 14:30:50 -0600

    v1

--- vidplayer/routes/movies.js
+++ vidplayer/routes/movies.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const mongodb = require('mongodb');	
+const fs = require("fs");	
 const router = express.Router()
 const Movie = require('../models/movie')
 const Genre = require('../models/genre')
 const imageMimeTypes = ['image/jpeg', 'image/png', 'images/gif']
-
+const url = 'mongodb://localhost:27017/vidplayer';
 // All Movies Route
 router.get('/', async (req, res) => {
   let query = Movie.find()
@@ -41,7 +43,19 @@ router.post('/', async (req, res) => {
     description: req.body.description
   })
   saveCover(movie, req.body.cover)
+mongodb.MongoClient.connect(url, function (error, client) {
+    if (error) {
+      res.status(500).json(error);
+      return;
+    }
+	const db = client.db('vidplayer');
+    const bucket = new mongodb.GridFSBucket(db);
+    const videoUploadStream = bucket.openUploadStream(req.body.title);
+    const videoReadStream = fs.createReadStream('./public/video/bigbuck.mp4');
+	videoReadStream.pipe(videoUploadStream);
+	console.log("upload success")
 
+});
   try {
     const newMovie = await movie.save()
     res.redirect(`movies/${newMovie.id}`)
@@ -61,6 +75,60 @@ router.get('/:id', async (req, res) => {
     res.redirect('/')
   }
 })
+//play route
+router.get('/:id/play', async (req, res) => {
+	try {
+    const movie = await Movie.findById(req.params.id)
+    res.render('movies/play', { movie: movie })
+  } catch {
+    res.redirect('/')
+  }
+})
+//play helper route
+router.get("/:id/play/mongo-video", function (req, res) {
+mongodb.MongoClient.connect(url, function (error, client) {
+    if (error) {
+      res.status(500).json(error);
+      return;
+    }
+	console.log("conneted to retrieve")
+	const db = client.db('vidplayer');
+    const range = req.headers.range;
+    if (!range) {
+      res.status(400).send("Requires Range header");
+    }
+
+    db.collection('fs.files').findOne({}, (err, video) => {
+      if (!video) {
+        res.status(404).send("No video uploaded!");
+        return;
+      }
+      // Create response headers
+      const videoSize = video.length;
+      const start = Number(range.replace(/\D/g, ""));
+      const end = videoSize - 1;
+
+      const contentLength = end - start + 1;
+      const headers = {
+        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": contentLength,
+        "Content-Type": "video/mp4",
+      };
+
+      // HTTP Status 206 for Partial Content
+      res.writeHead(206, headers);
+
+      const bucket = new mongodb.GridFSBucket(db);
+      const downloadStream = bucket.openDownloadStreamByName('bigbuck', {
+        start
+      });
+
+      // Finally pipe video to response
+      downloadStream.pipe(res);
+    });
+  });
+});
 
 // Edit Movie Route
 router.get('/:id/edit', async (req, res) => {
@@ -144,11 +212,8 @@ async function renderFormPage(res, movie, form, hasError = false) {
 }
 
 function saveCover(movie, coverEncoded) {
-  if (coverEncoded == null) return
-  {
   const cover = JSON.parse(coverEncoded)
-  console.log(cover)
-  }
+
   if (cover != null && imageMimeTypes.includes(cover.type)) {
     movie.coverImage = new Buffer.from(cover.data, 'base64')
     movie.coverImageType = cover.type
